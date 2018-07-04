d28781bd011a771f5d38d3197167a49ac6b2cd11
castchise <feelmoredrama@gmail.com>
2018-07-04 18:01:18 +0300

    mobile menu updt

--- ds/js/script.js
+++ ds/js/script.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 500);
     });
 });
+
+$(document).ready(function(){
+    $("#mobile").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
+
+function closeMenu(){
+  menu = document.getElementById('mobile-nav');
+  console.log(menu);
+
+  return menu.style.top = '-100%';
+}
+
+function openMenu(){
+  menu = document.getElementById('mobile-nav');
+  console.log(menu);
+
+  return menu.style.top = '0%';
+}
