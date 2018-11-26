08278269db4a9dc27557f4eebab233b950ce2fa0
castchise <feelmoredrama@gmail.com>
2018-11-26 08:34:27 +0200

    testform 1st push

---
+++ testform/js/cpscript.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// function getData(url) {
+//   let xhr = new XMLHttpRequest;
+//   xhr.open('GET', url, true);
+//   xhr.send();
+//   xhr.onreadystatechange = function() {
+//     if(xhr.status === 200 && xhr.readyState === 4){
+//       console.log(JSON.parse(xhr.responseText));
+//     }
+//   }
+// }
+
+// getData('http://codeit.pro/codeitCandidates/serverFrontendTest/news/getList');
+fillList();
+
+function fillList() {
+  document.querySelectorAll('.cp-loader').forEach(item => {
+    item.style.display = 'block';
+  });
+  let xhr = new XMLHttpRequest;
+  xhr.open('GET', 'http://codeit.pro/codeitCandidates/serverFrontendTest/company/getList', true);
+  setTimeout(() => { xhr.send(); }, 1000); // timeout just for preloader example
+  xhr.onreadystatechange = function() {
+    let out = '';
+    if(xhr.status === 200 && xhr.readyState === 4){
+      document.querySelectorAll('.cp-loader').forEach(item => {
+        item.style.display = 'none';
+      });
+
+      const response = JSON.parse(xhr.responseText);
+
+      let i = 0;
+      const counter = setInterval(() => {
+        if (i < response.list.length) {
+          document.querySelector('#company-count').innerHTML = ++i;
+        } else {
+          clearInterval(counter);
+        }
+      }, 3000 / 250);
+
+      response.list.forEach(item => {
+        console.log(item);
+        out += `<div class="company">${item.name}</div>`;
+      });
+
+      document.querySelector('.companies-container').innerHTML = out;
+
+      document.querySelectorAll('.company').forEach(item => {
+        item.addEventListener('click', function() {
+          let out = '';
+          response.list.forEach(company => {
+            if(this.innerHTML === company.name) {
+              console.log(company);
+              company.partners.forEach(partner => {
+                out += `
+                  <div class="partner">
+                    <div class="partner-info">
+                      <div class="name">${partner.name}</div>
+                      <div class="value">${partner.value}%</div>
+                    </div>
+                  </div>
+                `;
+                console.log(partner);
+              });
+              document.querySelector('.row.hidden').style.display = 'block';
+              document.querySelector('.partners-container').innerHTML = out;
+            }
+          });
+        });
+      });
+    }
+  }
+}
+
+// function barChart() {
+//   var ctx = document.getElementById('myChart').getContext('2d');
+//   ctx.canvas.height = 100;
+//   ctx.canvas.width = 200;
+//   var chart = new Chart(ctx, {
+//     type: 'pie',
+//     data: {
+//         labels: ["January", "February", "March"],
+//         datasets: [{
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [1, 10, 5],
+//         }]
+//     },
+//     options: {
+//     }
+//   });
+  
+//   console.log(ctx);
+// }
+
+// barChart();
---
+++ testform/js/script.js
@@ -0,0 +1,95 @@
+"use strict";
+
+let flag = false; // bool flag for form validation checking
+
+const firstName = document.getElementById('firstname'),
+      lastName = document.getElementById('lastname'),
+      email = document.getElementById('email'),
+      gender = document.getElementById('gender'),
+      pass = document.getElementById('password'),
+      checker = document.getElementById('checker');
+
+const register = document.getElementById('reg-form');
+
+register.addEventListener('submit', submitData);
+
+function submitData(e) {
+  checkIfEmpty(firstName.value, lastName.value, email.value, pass.value, checker); // check fields -> change flag state if all of them are filled
+  
+  
+  if (flag) {
+    document.querySelector('#loader').style.display = 'block';
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://codeit.pro/codeitCandidates/serverFrontendTest/user/registration', true);
+    xhr.send(new FormData(register));
+    console.log(new FormData(register));
+    xhr.onreadystatechange = function() {
+      if(xhr.status === 200 && xhr.readyState === 4) {
+        document.querySelector('#loader').style.display = 'none';
+        let response = JSON.parse(xhr.responseText);
+        console.log(response);
+        if(response.message !== 'User created') {
+          alertBox(`${response.message}`, `error`);
+        } else {
+          alertBox(`${response.message}`, `succ`);
+          setTimeout(function() {
+            window.location.replace('cp.html');
+          }, 2000);
+        }
+      } else if (xhr.status !== 200) {
+        console.log(`Error ${xhr.status} - ${xhr.statusText}`);
+      }
+    }
+
+  }
+
+  e.preventDefault();
+}
+
+// alert box message template
+function alertBox(msg, className) {
+  clearAlert();
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(msg));
+
+  const card = document.querySelector('.card');
+  const form = document.querySelector('#reg-form');
+  card.insertBefore(div, form);
+
+  setTimeout(function() { // clear alert box message in 3 sec
+    this.clearAlert();
+  }, 3000);
+}
+
+function clearAlert() {
+  const currenAlert = document.querySelector('.alert');
+  if(currenAlert) {
+    currenAlert.remove();
+  }
+}
+
+// highlighted field template
+function errorFocus(field){
+  field.classList.add('error-focus');
+
+  setTimeout(function() {
+    field.classList.remove('error-focus');
+  }, 2000);
+}
+
+function checkIfEmpty(name, lastname, email, pw, cond) {
+  if(name === '' || lastname === '' || email === '' || pw === '') {
+    alertBox(`Please, fill all fields`, `error`);
+    flag = false;
+  } else if (cond.checked !== true) {
+    alertBox(`agree with the terms`, `error`);
+    flag = false;
+  } else if (/\d/.test(name) || /\d/.test(lastname)) {
+    alertBox(`Please, enter correct name/lastname`, `error`);
+    flag = false;
+  } else {
+    console.log('all fields are filled and checkbox pressed');
+    flag = true;
+  }
+}
